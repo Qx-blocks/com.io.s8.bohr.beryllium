7858e9f35a99b671566af5836236572ea1f40b3f
Pierre Convert <pierreconvert@macbook-pro-6.lan>
2023-04-03 19:02:19 +0200

    Beryllium is now standalone

---
+++ sources/module-info.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+/**
+ * @author pierreconvert
+ *
+ */
+module com.s8.io.bohr.beryllium {
+	
+
+	/* <beryllium> */
+	exports com.s8.io.bohr.beryllium.codebase;
+	exports com.s8.io.bohr.beryllium.fields;
+	exports com.s8.io.bohr.beryllium.object;
+	exports com.s8.io.bohr.beryllium.syntax;
+	exports com.s8.io.bohr.beryllium.tables;
+	exports com.s8.io.bohr.beryllium.types;
+	/* </beryllium> */
+	
+	requires transitive com.s8.io.bohr.atom;
+	requires transitive com.s8.io.bytes;
+	
+}
