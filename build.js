ff7537f1562e7fe68aa408a9e4ed57b98424156f
Pierre Convert <pierreconvert@192.168.1.14>
2025-01-26 17:10:31 +0100

    Updating build.js

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const ROOT = build({
 	module: "com.s8.core.bohr.beryllium",
 	dependencies: [ "S8-api", "S8-core-io-bytes", "S8-core-bohr-atom"],
-	target: "S8-core-bohr-beryllium",
+	target: "S8-core-bohr-beryllium"
 });
